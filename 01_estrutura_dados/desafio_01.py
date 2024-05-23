103d3998afbeb51119425096e0d422caad53f120
Bruno DallAgnol <brunomorpheu@gmail.com>
2024-05-23 11:52:07 -0300

    updated - create criar_usuario(), filtrar_usuario() functions

--- 01_estrutura_dados/desafio_01.py
+++ 01_estrutura_dados/desafio_01.py
@@ -52,6 +52,26 @@ def exibir_extrato(saldo, /, *, extrato):
     print(f"Saldo:\t\tR$ {saldo:.2f}")
     print("====================================")
 
+def criar_usuario(usuarios):
+    cpf = input("Imforme o CPf (somente números): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n==> Já existe usuário com esse CPF! <==")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereço": endereco})
+
+    print("==> Usuário criado com sucesso! <==")
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
 # =============== end functions
 
 def main():
