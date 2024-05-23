f8e7f5b058de4c029d3228fe4c605e48c897ae67
Bruno DallAgnol <brunomorpheu@gmail.com>
2024-05-23 10:59:42 -0300

    updated - create depositar() function

--- 01_estrutura_dados/desafio_01.py
+++ 01_estrutura_dados/desafio_01.py
@@ -14,7 +14,15 @@ def menu():
     return input(textwrap.dedent(menu))
 
 # =============== functions
-
+def  depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:\tR$ {valor:.2f}\n"
+        print("\n==> Depósito realizado com sucesso! <==")
+    else:
+        print("\n==> Operação falhou! O valor informado é inválido. <==")
+    
+    return saldo, extrato
 
 
 # =============== end functions
